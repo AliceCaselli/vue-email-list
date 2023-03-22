691eb3f64b1b7a28df50aa4a5aab7cf257d9bfa8
AliceCaselli <alicecaselli3@gmail.com>
2023-03-22 22:10:42 +0100

    ciclo for

--- js/script.js
+++ js/script.js
@@ -6,3 +6,29 @@
 // Bonus
 // Far comparire gli indirizzi email solamente quando sono stati tutti generati.
 
+const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+        emails: [],
+      }
+    },
+
+    methods: {
+
+        getEmails(){
+
+            for( let i = 0; i < 10; i++){
+
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then ((res)=>{
+                    
+                    this.emails = res.data.response;
+                    console.log(res.data.response);
+                });
+
+            }
+        }
+    }
+
+  }).mount('#app')
